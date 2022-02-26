5e5eee3eac48eb0f208a9dd103b60c8cc33a23c6
ghiath <ghiatho@gmail.com>
2022-02-27 01:10:30 +0400

    python small letter

--- Heroku_Deployment/settings.py
+++ Heroku_Deployment/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-k)&vq=^q(^dqcqzf@hq&ujo_7l2$g5_nss#g_-w0q5g25gk+9w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
